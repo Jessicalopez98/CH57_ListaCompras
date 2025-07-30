fd0ea2e58800a77cb0d603514a7f30d2aad29e26
Jessica <jessica.lopez.perez.27@gmail.com>
2025-07-30 17:14:03 -0600

    Se cumplen puntos Validar la información de los campos

---
+++ js/main.js
@@ -0,0 +1,56 @@
+const txtName = document.getElementById("Name");
+const txtNumber = document.getElementById("Number");
+const btnAgregar = document.getElementById("btnAgregar");
+const btnClear = document.getElementById("btnClear");
+
+const alertValidaciones=document.getElementById("alertValidaciones");
+const alertValidacionesTexto=document.getElementById("alertValidacionesTexto");
+
+function validarCantidad(){
+    if(txtNumber.value==0){
+        return false;
+    }//validar que tenga info/ sea un num>0
+
+    if(isNaN(txtNumber.value)){
+        return false;
+    }//tiene que ser un num
+
+    if(Number(txtNumber.value)<=0){
+        return false;
+    }//Mayor que 0
+
+    return true;
+
+}//validarCantidad
+
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault();
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtName.style.border="";
+    txtNumber.style.border="";
+
+    //name - validar que tenga info min 3 letras
+    // num - que sea mayor que 0
+    if(txtName.value.length<3){
+        txtName.style.border= "medium red solid"
+        
+//mensaje error
+        alertValidacionesTexto.innerHTML=
+            "<strong> El nombre del producto no es correcto </strong>";
+        alertValidaciones.style.display = "block";
+    }//<3
+
+    if(! validarCantidad()){
+        txtNumber.style.border= "medium red solid"
+        alertValidacionesTexto.innerHTML +=
+        "<strong>La Cantidad no es correcta</strong>"
+        alertValidaciones.style.display="block" ;
+
+    }//! validarCantidad
+
+});//<3 btn agregar click
+
+
+
