46d180caf39ec8df0e868a106712449aff676a6e
Jessica <jessica.lopez.perez.27@gmail.com>
2025-08-01 13:38:20 -0600

    Se muestra info almacenada al recargar pagina,y funciones btnClear

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
     let isValid = true;
     alertValidacionesTexto.innerHTML="";
-    alertValidaciones.style.display="none";
+    alertValidaciones.style.display="none";//campos de limpieza de alerts y campos
     txtName.style.border="";
     txtNumber.style.border="";
 
@@ -90,6 +90,7 @@ btnAgregar.addEventListener("click", function(event){
 
     datos.push(elemento);//sube datos al final del arreglo //metodo rápido conejo igual que pop
     localStorage.setItem("datos", JSON.stringify(datos));//se pasa un objeto a string para ser usado en localStorage
+//lineas  85-92 para arreglo "datos" almacenado en localStorage.
 
     cuerpoTabla.insertAdjacentHTML("beforeend",row);
     contadorProductos.innerText=cont;
@@ -105,7 +106,8 @@ btnAgregar.addEventListener("click", function(event){
         "totalProductos" : totalProductos,
         "costoTotal" : costoTotal,
     };
-    localStorage.setItem("resumen", JSON.stringify(resumen));
+    localStorage.setItem("resumen", JSON.stringify(resumen));//JSON.stringify es para volver el objeto o numero a string.
+    //lineas 16-20 (para establecer variables), 105-109 (funcion). realizadas para guardar resumen en localStorage
 
 
     txtName.value = "";
@@ -116,5 +118,67 @@ btnAgregar.addEventListener("click", function(event){
 
 });//<3 btn agregar click
 
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    if(this.localStorage.getItem("datos")!=null){
+        datos = JSON.parse(this.localStorage.getItem("datos"));//hechizo para cambiar tipo de dato a string localStorage
+        datos.forEach( (dato) =>{
+            let row= `<tr>
+                <td>${dato.cont}</td>
+                <td>${dato.nombre}</td>
+                <td>${dato.cantidad}</td>
+                <td>${dato.precio}</td>
+            </tr>
+        `;
+         cuerpoTabla.insertAdjacentHTML("beforeend",row);
+        }); //forEach arreglo
+    } //datos != null que variable sea diferente de null, debe entregar string en local storage, pero queremos que regrese numeros
+    //
+
+    if(this.localStorage.getItem("resumen")!=null){
+    let resumen = JSON.parse(this.localStorage.getItem("resumen"));
+    costoTotal = resumen.costoTotal;
+    totalProductos = resumen.totalProductos;
+    cont = resumen.cont;
+    } //resumen != null que variable sea diferente de null, debe entregar string en local storage, pero queremos que regrese numeros
+
+    contadorProductos.innerText=cont;
+    productosTotal1.innerText = totalProductos;
+    precioTotal.innerText = new Intl.NumberFormat("es-MX", 
+                  { style: "currency", currency: "MXN" }).format(costoTotal);
+});//window load
+
+/*Accionar boton limpiar
+limpiar tabla, limpiar resumen, limpiar borde de campos,
+limpiar alerts,limpiar datos y resumen guardado en localStorage */
+
+btnClear.addEventListener("click", function(event){
+    event.preventDefault();
+
+    cuerpoTabla.innerHTML= ""; //limpiando tabla
+
+    txtName.value = "";
+    txtNumber.value = "";//limpiando los campos
+    txtName.focus();
+
+    alertValidacionesTexto.innerHTML="";
+    alertValidaciones.style.display="none";
+    txtName.style.border="";
+    txtNumber.style.border="";//limpiando campos y alerts
 
 
+    cont = 0;
+    costoTotal = 0;
+    totalProductos = 0;
+
+    contadorProductos.innerText=cont;
+    productosTotal1.innerText = totalProductos;
+    precioTotal.innerText = new Intl.NumberFormat("es-MX", 
+                  { style: "currency", currency: "MXN" }).format(costoTotal);//borrar resumen 170-177
+    
+    datos = new Array(); //borrar datos
+
+    //eliminando local storage
+    localStorage.removeItem("datos");
+    localStorage.removeItem("resumen");
+});//btnClear
